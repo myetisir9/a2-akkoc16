d5df789c88a49d010b34b02fb2ee8491e9fd5e36
akkoc16 <32218770+akkoc16@users.noreply.github.com>
2018-12-30 22:21:29 +0300

    Update bottle_app.py

--- bottle_app.py
+++ bottle_app.py
@@ -6,6 +6,16 @@
 #####################################################################
 
 from bottle import route, run, default_app, debug, static_file
+global comments
+
+
+def html_comments():
+	global comments
+	text = '<ul>'
+	for i in range(0,len(comments)):
+		text += '<li class="message">'+comments[i]+'</li>'
+	text += '</ul>'
+	return text
 
 def htmlify(title,text):
     page = """
